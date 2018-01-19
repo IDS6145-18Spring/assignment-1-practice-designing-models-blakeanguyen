579ba7080e9dc4d6468f37e260340b14e58d1566
blakeanguyen <34401894+blakeanguyen@users.noreply.github.com>
2018-01-18 23:05:41 -0500

    Add files via upload
    Recyclebot codes

---
+++ code/recyclebot.py
@@ -0,0 +1,30 @@
+from recyclebot import recyclebot
+
+class recyclebot(recyclebot): 
+    '''This is a recycling autonomous agent'''
+
+    def __init__(self):
+        '''Intializes the recyclebot'''
+        self.plastic = name
+        self.amount.of.plastic = float
+        self.paper = name
+        self.amount.of.paper = float
+        self.metal = name
+        self.amount.of.metal = float
+        robot.__init__(self)
+        
+    def destroy(self):
+        '''The robot will destroy all non-recyclable waste'''
+        return math.pi * self.amount.of.plastic ** 2 ** self
+    
+    def repurpose(self):
+        '''This is how recyclebot repurposes recycled materials'''
+        self.plastic.type = string
+        self.plastic.density = float
+        self.paper.type = string
+        self.paper.density = float
+        self.metal.type = string
+        self.metal.density = float
+        return None
+    
+        
---
+++ code/repurpose.py
@@ -0,0 +1,23 @@
+from recyclebot import repurpose
+
+class repurpose (plastic, paper, metal):
+    def __init__(self):
+            self.plastic = 
+            self.amount.of.plastic =
+            self.paper = 
+            self.amount.of.paper = 
+            self.metal = 
+            self.amount.of.metal =
+
+    def plastic(self):
+        def plastic.type =
+        def density.type =
+
+    def paper(self):
+        def paper.type =
+        def density.type =
+
+    def metal(self):
+        def metal.type =
+        def density.type =
+ 
